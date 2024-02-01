00a7f5d4dba815d91f61ff0ec2bc9870323be8cc
Douglas Stanbro <dstanbr@wgu.edu>
2024-01-31 21:28:59 -0600

    Initial Commit

---
+++ Cookcademy 1/Cookcademy_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Cookcademy_1App.swift
+//  Cookcademy 1
+//
+//  Created by Douglas Stanbro on 1/31/24.
+//
+
+import SwiftUI
+
+@main
+struct Cookcademy_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
