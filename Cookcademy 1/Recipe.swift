1cfeb70304f25dd5f5810895cbb23b4a4cf659a0
Douglas Stanbro <dstanbr@wgu.edu>
2024-01-31 21:54:57 -0600

    quick update to utils

---
+++ Cookcademy 1/Recipe.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe.swift
+//  Cookcademy 1
+//
+//  Created by Douglas Stanbro on 1/31/24.
+//
+
+import Foundation
+
+struct Recipe {
+    var name: String
+    var description: String
+    var author: String
+    var category: String
+    var ingredients: [String]
+    var directions: [String]
+}
