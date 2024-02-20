6ed50d32a15192b52149ba7084d578677c4a806c
Douglas <dstanbr@wgu.edu>
2024-02-20 11:00:50 -0600

    this is just an update to files

--- Cookcademy 1/Recipe.swift
+++ Cookcademy 1/Recipe.swift
@@ -1,47 +1,58 @@
 import Foundation
 
-struct reciepe {
-
-   var ingredients: [Ingredient]
-   var directions: [Direction]
-   var mainInformation: MainInformation
+struct Recipe {
+    var mainInformation: MainInformation
+    var ingredients: [Ingredient]
+    var directions: [Direction]
 }
-
 
 struct MainInformation {
-   var name: String
-   var description: String
-   var author: String
-   var category: Category 
+    var name: String
+    var description: String
+    var author: String
+    var category: Category
 
-   
-   enum Category: String, CaseIterable {
-
-      case breakfast = "Breakfast"
-      case lunch = "Lunch"
-      case dinner = "Dinner"
-      case dessert = "Dessert"
-   }
+    enum Category: String, CaseIterable {
+        case breakfast = "Breakfast"
+        case lunch = "Lunch"
+        case dinner = "Dinner"
+        case dessert = "Dessert"
+    }
 }
-
 
 struct Ingredient {
-   var name: String
-   var quantity: Double
-   var unit: String
+    var name: String
+    var quantity: Double
+    var unit: Unit
 
-   enum Unit: String, CaseIterable {
-      case oz = "Ounces"
-      case g = "Grams"
-      case cups = "Cups"
-      case tbs = "Tablespoons"
-      case none = "No units"
-   }
+    var desciption: String {
+        let formattedQuantity = String(format: "%g", quantity)
+        switch unit {
+        case .none:
+            let formattedName = quantity == 1 ? name : "\(name)s"
+            return "\(formattedQuantity) \(formattedName)"
+        default:
+            if quantity == 1 {
+                return "1 \(unit.singularName) \(name)"
+            } else {
+                return "\(formattedQuantity) \(unit.rawValue) \(name)"
+            }
+        }
+    }
+
+    enum Unit: String, CaseIterable {
+        case oz = "Ounces"
+        case g = "Grams"
+        case cups = "Cups"
+        case tbs = "Tablespoons"
+        case tsp = "Teaspoons"
+        case none = "No units"
+
+        var singularName: String { String(rawValue.dropLast()) }
+    }
 }
-struct Disection {
 
-   var description: String
-   var isOptional: Bool
-
+struct Direction {
+    var description: String
+    var isOptional: Bool
 }
-
